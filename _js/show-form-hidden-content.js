c707c59871eda1514643832edb35a14bc48c1611
Ryanalvess <ryanalves849@gmail.com>
2021-05-22 13:19:36 -0300

    changes 22/05/21
    alterações no header, criação da página de login e mudança de endereço do repositório local

---
+++ _js/show-form-hidden-content.js
@@ -0,0 +1,42 @@
+const showPass = () => 
+{
+    const iptPass = document.getElementById('iptPass')
+    
+    if( iptPass.type === "password" )
+        iptPass.type = "text"
+    else
+        iptPass.type = "password"
+}
+
+const showAdmPass = () => 
+{
+    const iptAdmPass = document.getElementById('iptAdmPass')
+    
+    if( iptPass.type === "password" )
+        iptPass.type = "text"
+    else
+        iptPass.type = "password"
+}
+
+// mostra o campo do código do administrador e altera o icon do botão
+const showAdmField = () =>
+{
+    const btn = document.querySelector('.dev-login-btn')
+    const admField = document.querySelector('.admField')
+    const icons = [
+        'url(../_img/_svg/icons/devicon.svg)',
+        'url(../_img/_svg/icons/user.svg)'
+    ];
+
+    if( admField.style.display === "none" ){
+        admField.style.display = "block"
+        btn.style.backgroundImage = icons[1]
+        btn.style.backgroundSize = "22px"
+    }else{
+        admField.style.display = "none"
+        btn.style.backgroundImage = icons[0]
+        btn.style.backgroundSize = "12px"
+    }
+
+}
+
